9626e85a697c8e14da7a6199549fba47349d8d5c
lavanya_r <Lavanya_R@Dellteam.com>
2022-03-25 11:16:46 +0530

    Initial Commit

---
+++ BasicCSharp/DataTypes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCSharp
+{
+    class DataTypes
+    {
+        public void Do()
+        {
+            int a = 1;
+            long b = 123345;
+            float c = 12.34F;
+            double d = 1234.55;
+            decimal e = 23333333333333.34M;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+        }
+    }
+}
---
+++ BasicCSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BasicCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            DataTypes d = new DataTypes();
+            d.Do();
+        }
+    }
+}
